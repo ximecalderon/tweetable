5a4db8c3a1e7faa63fd257fc09a69f14c2d0138f
Ximena Calderón <86806702+ximecalderon@users.noreply.github.com>
2022-02-03 00:12:49 -0500

    Seeded initial users and tweets

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,47 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require "faker"
+
+puts "Seeding users and tweets..."
+
+Tweet.delete_all
+User.delete_all
+
+admin = User.create(email: "admin@mail.com",
+                    username: "admin",
+                    name:"Admin the Great",
+                    password: "letmein",
+                    role: 1)
+
+team = [["ximecalderon", "Ximena Calderón"],
+        ["sakuta", "Luis Chota"],
+        ["pedro_art", "Pedro Aruata"],
+        ["cowalsky", "Antony Rojas"]]
+
+team.each do |username, name|
+  user = User.new(email: "#{name.split.first.downcase}@mail.com",
+                  username: username,
+                  name:name,
+                  password: "letmein",
+                  role: 0)
+  user.avatar.attach(io: URI.open(Faker::Avatar.unique.image(size: "100x100", format: "jpg")), filename: "#{username}.jpg")
+  p user.errors.full_messages unless user.save
+
+  tweet = Tweet.create(body: Faker::Quote.unique.famous_last_words, user_id: user.id)
+end
+
+puts "Seeding of #{User.all.size} users and #{Tweet.all.size} tweets completed."
+
+puts "Seeding replies..."
+Tweet.all.each do |tweet|
+  users = User.all.to_a
+  users.delete(tweet.user)
+
+  users.each do |user|
+    reply = Tweet.create(body: Faker::Quote.unique.famous_last_words, user_id: user.id)
+    tweet.replies.push(reply)
+  end
+end
+
+puts "Seeding of replies completed."
+
+puts "Seeding likes..."
+puts "Seeding likes completed."
