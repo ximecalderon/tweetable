be7cfbe237e82741e8d7af6496de8f2ae1e577ee
luisftime <luisftime@gmail.com>
2022-02-05 13:24:10 -0500

    fix error

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,5 @@
 class TweetsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: %i[show index]
   before_action :set_tweet, only: %i[show edit update destroy like like_destroy]
 
   # GET /tweets
